7dec09bb5edd278d71d350f810d6ed67b44b8748
Le Quang Thai Son <sonlqt.work@gmail.com>
2026-02-04 20:41:55 +0700

    Update: Update call API for venue detail

--- lib/providers/venue_detail_provider.dart
+++ lib/providers/venue_detail_provider.dart
@@ -1,18 +1,32 @@
+import 'package:couple_mood_mobile/models/api_response.dart';
 import 'package:couple_mood_mobile/models/venue_model.dart';
 import 'package:couple_mood_mobile/services/venue_service.dart';
 import 'package:flutter/material.dart';
 
 class VenueDetailProvider extends ChangeNotifier {
-  Venue? venue;
+  ApiResponse<Venue>? venueResponse;
   bool loading = false;
+  String? error;
 
-  Future<void> loadVenue(int id) async {
+  Venue? get venue => venueResponse?.data;
+
+  Future<void> loadVenue() async {
     loading = true;
+    error = null;
     notifyListeners();
 
-    venue = await VenueService.getVenueDetail(1);
+    try {
+      //hardcode test, nhớ remove huhu
+      venueResponse = await VenueService.getVenueDetail(1);
 
-    loading = false;
-    notifyListeners();
+      if (venueResponse!.code != 200 || venueResponse!.data == null) {
+        error = venueResponse!.message;
+      }
+    } catch (e) {
+      error = e.toString().replaceFirst('Exception: ', '');
+    } finally {
+      loading = false;
+      notifyListeners();
+    }
   }
 }
--- lib/services/venue_service.dart
+++ lib/services/venue_service.dart
@@ -1,21 +1,18 @@
+import 'package:couple_mood_mobile/models/api_response.dart';
 import 'package:couple_mood_mobile/models/venue_model.dart';
 
 import 'api_client.dart';
 
 class VenueService {
-  static Future<Venue> getVenueDetail(int id) async {
+  static Future<ApiResponse<Venue>> getVenueDetail(int id) async {
     final res = await ApiClient.request(
       '/VenueLocation/$id',
       method: HttpMethod.get,
     );
 
-    final root = (res as Map).cast<String, dynamic>();
-
-    if (root['code'] != 200) {
-      throw Exception(root['message']?.toString() ?? 'Lỗi lấy venue');
-    }
-
-    final data = (root['data'] as Map).cast<String, dynamic>();
-    return Venue.fromJson(data);
+    return ApiResponse<Venue>.fromJson(
+      res as Map<String, dynamic>,
+      (json) => Venue.fromJson(json as Map<String, dynamic>),
+    );
   }
 }
