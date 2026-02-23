6ac8da7b01170eeb7865abd9692f6572bf80eb3b
Nghia Pham <minidora2707@gmail.com>
2026-02-24 03:00:23 +0700

    loại bỏ các import thừa

--- lib/screens/coupleInvitation/widget/search_member/user_card.dart
+++ lib/screens/coupleInvitation/widget/search_member/user_card.dart
@@ -1,8 +1,6 @@
 import 'package:couple_mood_mobile/models/coupleInvitation/member_response.dart';
-import 'package:couple_mood_mobile/providers/couple_invitation_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
 
 class UserCard extends StatelessWidget {
   final void Function(String message) onSend;
