c6002b9318586411b7561fd1ebaecd6b4431f3c6
Nghia Pham <minidora2707@gmail.com>
2026-04-18 17:07:50 +0700

    Fix lại tràn viền ở home phần khu vự phổ biến

--- lib/screens/home/widget/popular_nearby.dart
+++ lib/screens/home/widget/popular_nearby.dart
@@ -24,6 +24,8 @@ class PopularNearby extends StatelessWidget {
             children: [
               const Text(
                 "Khu vực phổ biến gần bạn",
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
               TextButton(
