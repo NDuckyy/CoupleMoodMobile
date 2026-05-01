3eef52d69bb8a2288543d348d10816a7356fe447
Nghia Pham <minidora2707@gmail.com>
2026-05-01 23:59:20 +0700

    Fix lại tên tỉnh thành

--- lib/screens/location/list_location_screen.dart
+++ lib/screens/location/list_location_screen.dart
@@ -83,19 +83,42 @@ class _ListLocationScreenState extends State<ListLocationScreen> {
 
         lat = position.latitude;
         lng = position.longitude;
-      } else {
+      } else if (source == LocationSource.partner) {
         final partnerId = moodProvider.coupleCurrentMood?.partnerMemberId;
         if (partnerId == null) return;
 
         await positionProvider.getUserPosition(partnerId);
 
         if (positionProvider.error != null) {
+          if (!mounted) return;
           showMsg(context, "Không lấy được vị trí đối phương", false);
           return;
         }
 
         lat = positionProvider.recommendedLatitude;
         lng = positionProvider.recommendedLongitude;
+      } else if (source == LocationSource.middle) {
+        final myPos = await LocationService.getCurrentPosition();
+        final partnerId = moodProvider.coupleCurrentMood?.partnerMemberId;
+
+        if (myPos == null || partnerId == null) return;
+
+        await positionProvider.getUserPosition(partnerId);
+
+        if (positionProvider.error != null) {
+          if (!mounted) return;
+          showMsg(context, "Không lấy được vị trí đối phương", false);
+          return;
+        }
+
+        final partnerLat = positionProvider.recommendedLatitude;
+        final partnerLng = positionProvider.recommendedLongitude;
+
+        if (partnerLat == null || partnerLng == null) return;
+
+        /// 🔥 midpoint đơn giản
+        lat = (myPos.latitude + partnerLat) / 2;
+        lng = (myPos.longitude + partnerLng) / 2;
       }
 
       recommendationProvider.latitude = lat;
@@ -105,6 +128,7 @@ class _ListLocationScreenState extends State<ListLocationScreen> {
         RecommendationRequest(lat: lat, lng: lng),
       );
     } catch (e) {
+      if (!mounted) return;
       showMsg(context, "Lỗi lấy vị trí", false);
     } finally {
       if (mounted) {
